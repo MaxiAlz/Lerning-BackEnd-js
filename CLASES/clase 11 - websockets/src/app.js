b5d7d7b4e5c25f8ef4f667522d79d22ca7306e24
Maxi Alzugaray <maxialz99@hotmail.com>
2022-12-22 14:13:24 -0300

    actualizo con clase crud mongo

---
+++ CLASES/clase 11 - websockets/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import __dirname from './utils.js'
+import handlebars from 'express-handlebars'
+import { Server } from 'socket.io'
+
+const app = express()
+
+const httpServer = app.listen(8080, () => console.log('Listeing...'))
+const socketServer = new Server(httpServer)
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+app.use(express.static(__dirname + '/public'))
+app.use('/', (req, res) => res.send('ok'))
